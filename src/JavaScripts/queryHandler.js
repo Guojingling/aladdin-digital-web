247a19c919a6ed087d56d38f35da2fd6246f551a
Jing Guo <guojingling@hotmail.com>
2023-02-11 22:44:27 +1100

    not to submit by default.

--- src/JavaScripts/queryHandler.js
+++ src/JavaScripts/queryHandler.js
@@ -1,4 +1,5 @@
 function testResults (form) {
+    preventDefault();
     var name = form.name.value;
     alert ("Thanks for your query, " + name) + ". We will get back to you shortly";
     location.reload();
