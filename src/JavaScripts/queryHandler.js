f63990e6b54e81f1788a296f6f07e908a21f18d5
Jing Guo <guojingling@hotmail.com>
2023-02-11 22:34:28 +1100

    update JavaScript to reload page.

--- src/JavaScripts/queryHandler.js
+++ src/JavaScripts/queryHandler.js
@@ -1,4 +1,5 @@
 function testResults (form) {
     var name = form.name.value;
-    alert ("You typed: " + name);
+    alert ("Thanks for your query, " + name) + ". We will get back to you shortly";
+    location.reload();
 }
