b235693e2f14b849f1de2a7ccf79bea281cc3fc4
Jing Guo <guojingling@hotmail.com>
2023-02-12 09:31:55 +1100

    prevent submit

--- src/JavaScripts/queryHandler.js
+++ src/JavaScripts/queryHandler.js
@@ -2,5 +2,5 @@ function testResults (form) {
     var name = form.name.value;
     alert ("Thanks for your query, " + name) + ". We will get back to you shortly";
     location.reload();
-    preventDefault();
+    return false;
 }
