250c2d61b356186c1cb546a5eb9f5e03b521be15
Jing Guo <guojingling@hotmail.com>
2023-02-12 09:07:03 +1100

    Only show alert, no submit.

--- src/JavaScripts/queryHandler.js
+++ src/JavaScripts/queryHandler.js
@@ -1,6 +1,6 @@
-function testResults (form) {
-    preventDefault();
+function testResults (form) {    
     var name = form.name.value;
     alert ("Thanks for your query, " + name) + ". We will get back to you shortly";
     location.reload();
+    preventDefault();
 }
