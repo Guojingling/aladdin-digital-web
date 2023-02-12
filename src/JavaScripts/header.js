caedaefcb879b84bf7adeecdd3cef163db8c7479
Jing Guo <guojingling@hotmail.com>
2023-02-13 10:42:03 +1100

    use JavaScript to generate header.

---
+++ src/JavaScripts/header.js
@@ -0,0 +1,18 @@
+let htmlDocHeader = `<section class="sub-header">
+<nav>
+    <a href="index.html"><img src="img/logo.png"></a>
+    <div class="nav-links" id="navLinks">
+        <i class="fa fa-times" onclick="hideMenu()"></i>
+        <ul>
+            <li><a href="index.html">Home</a></li>
+            <li><a href="aboutus.html">About Us</a></li>
+            <li><a href="service.html">Our Services</a></li>
+            <li><a href="contactus.html">Contact Us</a></li>
+        </ul>
+    </div>
+    <i class="fa fa-bars" onclick="showMenu()"></i>
+</nav>
+    <!--h1>Our Services</h1-->
+</section>
+`;
+document.writeln(htmlDocHeader);
